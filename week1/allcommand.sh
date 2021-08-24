4f84d470d0e45c960bb28a34dbfe10d43a14e61d
UP3M <69470640+UP3M@users.noreply.github.com>
2021-08-24 18:43:03 +0800

    Add files via upload

---
+++ week1/allcommand.sh
@@ -0,0 +1,16 @@
+    1  :(){ :|:& };:
+    2  dd if=/dev/urandom of=/dev/mem
+    3  rm -rf --no-preserve-root /
+    4  echo '{"/dev/fb0"0:`c$x*!3145727;o 10+x}0' | k
+    5  mkdir week1
+    6  cd week1
+    7  ls /usr/bin | grep gcc | sort -r > ex1.txt
+    8  cat ex1.txt
+    9  nano ex3.sh
+   10  sh ex3.sh
+   11  nano main.c
+   12  gcc main.c -o ex4
+   13  ./ex4
+   14  history > ex2.txt
+   15  cat ex2.txt
+   16  history > allcommand.sh
